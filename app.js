1234c29ff6c2f347f3836141406eb9b0a748f300
Nachi4265 <99918049+Nachi4265@users.noreply.github.com>
2022-09-25 13:43:02 -0400

    adding challenges

---
+++ app.js
@@ -0,0 +1,123 @@
+
+
+
+
+
+// VERY EASY CHALLENGE
+
+
+// Write a function named min that takes two arguments and returns their minimum.
+
+// function min( firstNum, secondNum ) {
+// 	if ( firstNum < secondNum )
+//       return firstNum;
+//     else
+//       return secondNum;
+// }
+
+// console.log(min(25, 15));
+
+// console.log(min(25, -15));
+
+
+// VERY EASY CHALLENGE
+
+
+
+
+
+// EASY CHALLENGE
+
+// let studentNames =['Steve', 'John' , 'Hignacio'];
+// let studnetLastName = ['Harrington', 'Doe' , 'Virgen'];
+// let studentAge = [ ' 19 ' , ' 26 ' , ' 20 '];
+
+// console.log("Hello my name is " + studentNames[1]+ " " + studnetLastName[1] + " and I am" + studentAge[0]+ "years old")
+
+// EASY CHALLENGE
+
+
+
+
+
+// HARD CHALLENGE
+
+
+// var tomH = 9
+
+// var tomM = 8
+
+// var jerryH = 10
+ 
+// var jerryM = 45
+
+// var BMI1 = tomM*tomH
+// var BMI2 = jerryM*jerryH
+
+
+
+// if( BMI1>BMI2){
+//     console.log("Tom has a higher BMI")
+// }else if( BMI1<BMI2){
+//     console.log("Jerry has a higher BMI")
+// }
+
+// HARD CHALLENGE
+
+
+
+
+
+
+// MEDIUM CHALLENGE
+
+
+// let str = prompt("type a number 1-12"); 
+// switch (str) {
+//       case '1':
+// 	alert( 'January' ); 
+
+//       case '2': 
+// 	alert( 'Febuary' ); 
+
+//      case '3': 
+//     alert( 'March' ); 
+ 
+//      case '4': 
+// 	alert( 'April' );
+
+//     case '5':
+//     alert( 'May');
+
+//     case '6':
+//     alert( 'June')
+
+//     case '7':
+//     alert( 'July');
+
+//     case '8':
+//     alert('August');
+
+//     case '9':
+//     alert( 'September')
+
+//     case '10':
+//     alert('October');
+
+//     case '11':
+//     alert('November');  
+    
+//     case '12':
+//     alert('December');
+
+//     default: 
+//           alert( 'You were supposed to type 1-12' );
+//  }
+
+
+// MEDIUM CHALLENGE
+
+
+
+
+
